b27e14572719912ea237b91c729aa8028ae78419
Justin Berry <justinberry@MacBook-Pro.local>
2021-05-06 15:20:20 -0400

    achieved HTML file upload, and created CRUD operations for image repo

--- static/code.js
+++ static/code.js
@@ -1,14 +1,25 @@
 console.log('hello')
 
 
-let reader = new FileReader();
-
-
 
 const input = document.getElementById('img');
 console.log(input)
-
+var bstrings = [];
+// get binary strings for uploaded files (accepts multiple)
 input.addEventListener("change", function () {
+
     let files = this.files;
-    console.log(files);
+
+    for (i=0; i < files.length; i++) {
+        let reader = new FileReader();
+
+        reader.onloadend = function () {
+            bstrings.push(reader.result)
+        }
+        
+        reader.readAsDataURL(files[i]);
+        
+    }
+
+    console.log(bstrings);
 });
